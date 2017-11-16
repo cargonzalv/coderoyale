76593eaeb06dce5e7521b5b9fe52e3a8ae5c9b1b
JavierTrc <ja.troconis10@uniandes.edu.co>
2017-11-16 17:56:45 -0500

    Updated the SpecteList component with the desired layout, ACTUALLY added bootsrap

--- imports/ui/components/SpectateList.js
+++ imports/ui/components/SpectateList.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 class SpectateList extends React.Component{
+
+	showMatches(){
+		const matches = ['M1','M2']
+		return matches.map((d)=>{
+			return (
+				<li className="list-group-item d-flex justify-content-between align-items-center">
+          <h5 className="mb-1">{d}</h5>
+          <button className="btn">Spectate</button>
+				</li>
+			)
+		})
+	}
+
 	render(){
 		return(
 			<div className="col-md-6">
-				Spectator List
+				<h1>Current Matches</h1>
+				<ul className="list-group">
+				  {this.showMatches()}
+				</ul>
 			</div>
 		)
 	}
