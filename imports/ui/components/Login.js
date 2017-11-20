cc8b7c64c447da1052cff0cb336007f721e2a526
JavierTrc <ja.troconis10@uniandes.edu.co>
2017-11-20 10:01:47 -0500

    Added github authentication logic

--- imports/ui/components/Login.js
+++ imports/ui/components/Login.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Meteor } from 'meteor/meteor';
+import GitHub from 'github-api';
+
 
 class Login extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.gh = new GitHub();
+		this.loginGithub = this.loginGithub.bind(this);
+	}
+
+	loginGithub(){
+		Meteor.loginWithGithub((err) => {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log("Success on github");
+		    console.log(Meteor.user());
+		  }
+		});
+	}
+
 	render(){
 		return(
 			<div className="col-md-6">
-				Login
+				<p>
+					If you don't have a github account yet create one
+					<a href="https://github.com"> github.com</a>, otherwise
+					login through the following button
+				</p>
+				<button className="btn btn-lg" onClick={this.loginGithub}>
+					Github
+				</button>
 			</div>
 		)
 	}
--- server/main.js
+++ server/main.js
@@ -5,5 +5,15 @@ Meteor.startup(() => {
   Inject.rawModHtml("addLanguage", function(html) {
     return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="en">');
   });
+
+  ServiceConfiguration.configurations.upsert({
+	  service: "github"
+	}, {
+	  $set: {
+	    clientId: Meteor.settings.github.clientId,
+	    loginStyle: "popup",
+	    secret: Meteor.settings.github.secret
+	  }
+	});
   
 });
