31f3372c1693417cdf717ca65854ed657e326e87
babohorquez10 <ba.bohorquez10@uniandes.edu.co>
2017-12-12 03:28:10 -0500

    Accesibilidad
    Especificar el lenguaje es muy buena práctica para mejorar la accesibilidad.

--- server/main.js
+++ server/main.js
@@ -4,6 +4,8 @@ import { ActiveGame } from '../imports/api/active-games.js';
 import { HistoryGame} from '../imports/api/history-games.js';
 Meteor.startup(() => {
   // code to run on server at startup
+  
+  // Al agregar el lenguaje se mejora la accesibilidad. Muy bien.
   Inject.rawModHtml("addLanguage", function(html) {
   	return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="en">');
   });
